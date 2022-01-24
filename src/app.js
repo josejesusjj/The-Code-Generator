6712d020ea3b611f1ffe6af1f7bd117689f82137
Jose <josejesuscedeno@gmail.com>
2022-01-24 20:33:14 +0000

    no funciona, debo arreglarlo

--- src/app.js
+++ src/app.js
@@ -1,21 +1,8 @@
-function GenerateDomain(){
-  let pronoun = ['the','our','your','my','one'];
-  let adj = ['great','big','huge','best','super'];
-  let noun = ['jogger','racoon','fox','man','company','girl','site'];
-  let domain = ['com','es','eu','net'];
+let pronoun = ['the','our','your','my','one'];
+let adj = ['great','big','huge','best','super'];
+let noun = ['jogger','racoon','fox','man','company','girl','site'];
+let domain = ['.com','.es','.eu','.net'];
 
-  let pronounIndex = Math.floor(Math.random() * pronoun.length);
-  let adjIndex = Math.floor(Math.random() * adj.length);
-  let nounIndex = Math.floor(Math.random() * noun.length);
-  let domainIndex = Math.floor(Math.random() * domain.length);
-  
-  return (
-    pronoun[pronounIndex] +
-  
-    adj[adjIndex] +
-   
-    noun[nounIndex] +
-    "." +
-    domain[domainIndex])}
-
-console.log(GenerateDomain())
+for(let i=0; i<pronoun.length;1++) {
+  console.log(pronoun[i]);
+ }
