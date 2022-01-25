19e473d2a0917174a4e98a691a8c5afefb2b0e8e
Jose <josejesuscedeno@gmail.com>
2022-01-25 15:38:23 +0000

    da igual, esta todo mal aqui

--- src/app.js
+++
@@ -1,8 +0,0 @@
-let pronoun = ['the','our','your','my','one'];
-let adj = ['great','big','huge','best','super'];
-let noun = ['jogger','racoon','fox','man','company','girl','site'];
-let domain = ['.com','.es','.eu','.net'];
-
-for(let i=0; i<pronoun.length;1++) {
-  console.log(pronoun[i]);
- }
