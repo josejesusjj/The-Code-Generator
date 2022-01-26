6a682b7ead432490ca7439a1e1eaf51dd08e3191
Jose <josejesuscedeno@gmail.com>
2022-01-26 17:55:27 +0000

    Ejercicio finalizado

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-
 function domain() {
-    let a = ["nuestro","mi","tu"]
-    let b = ['piso','apartamento','chalet']
-    let c = [".com",".es",'.net']
+  let a = ["nuestro", "mi", "tu"];
+  let b = ["piso", "apartamento", "chalet"];
+  let c = [".com", ".es", ".net"];
 
-    for(let i=0;i<a.length;i++){
-        for(let j=0;j<b.length;j++){
-            for(let k=0;k<c.length;k++){
-                console.log(a[i]+b[j]+c[k]);
-            }            
-        }       
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      for (let k = 0; k < c.length; k++) {
+        document.write(a[i] + b[j] + c[k]);
+        document.write("<br/>");
+      }
     }
+  }
 }
 domain();
