48bc46dfce7958f2b586d980832d7b32bc5ae514
Jose <83340481+josejesusjj@users.noreply.github.com>
2022-01-26 18:34:08 +0100

    Update app.js
    agregada palabra clave let a variables for

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ function domain() {
     let b = ['piso','apartamento','chalet']
     let c = [".com",".es",'.net']
 
-    for(i=0;i<a.length;i++){
-        for(j=0;j<b.length;j++){
-            for(k=0;k<c.length;k++){
+    for(let i=0;i<a.length;i++){
+        for(let j=0;j<b.length;j++){
+            for(let k=0;k<c.length;k++){
                 console.log(a[i]+b[j]+c[k]);
             }            
         }       
